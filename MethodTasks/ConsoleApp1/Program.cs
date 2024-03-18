ad6f86a467f75ceb46925d1ef895888a439c2ec4
gurbanli11 <orkhan.80@bk.ru>
2024-03-18 17:46:39 +0400

    methods

---
+++ MethodTasks/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Min();
+            //Console.WriteLine(Area(8, 5));
+            //Console.WriteLine(Area(2,3, 5));
+            //Console.WriteLine(Area(1,2,3,4,5));
+
+        }
+        //public static void Min() 
+        //{
+        //    //int[] nums = { 5, 11, 5723, 13, 421, 87, 5724, -505 };
+        //    //int min = nums[0];
+        //    //for (int i = 0; i < nums.Length; i++)
+        //    //{
+        //    //    if (min > nums[i])
+        //    //    {
+        //    //        min = nums[i];
+        //    //    }
+        //    //}
+        //    //Console.WriteLine("En kicik eded " + min);
+        //}
+
+
+        //public static int Area(int a, int b)
+        //{
+        //    return a * b;
+        //}
+
+        //public static int Area(int a, int b, int c)
+        //{
+        //    return 2 * (a * b + a * c + b * c);
+        //}
+
+        //public static int Area(int a, int b, int c, int p, int r)
+        //{
+        //    return p * r;
+        //    return (a + b + c) / 2;
+        //}
+
+    }
+}
